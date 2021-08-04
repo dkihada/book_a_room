06488cfb757cbd7556aba2560ec855b9d101464b
Denis Agureev <dkihada@gmail.com>
2021-08-04 16:17:11 +0500

    UPD: add plugins / a devServer

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,81 @@
 const path = require('path'),
       HTMLWebpackPlugin = require('html-webpack-plugin'),
-      {CleanWebpackPlugin} = require('clean-webpack-plugin');
+      CopyPlugin = require('copy-webpack-plugin'),
+      {CleanWebpackPlugin} = require('clean-webpack-plugin'),
+      MiniCssExtractPlugin = require('mini-css-extract-plugin'),
+      OptimazeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin'),
+      TerserWebpackPlugin = require('terser-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development',
+      isProd = !isDev;
+
+const optimization = () => {
+  const config = {
+      splitChunks: {
+      chunks: 'all'
+    }
+  }
+
+  if(isProd) {
+    config.minimizer = [
+      new OptimazeCssAssetsWebpackPlugin(),
+      new TerserWebpackPlugin()
+    ]
+  }
+
+  return config
+}
+
+const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   mode: 'development',
   entry: './js/index.js',
   output: {
-    filename: '[name].[contenthash].js',
+    filename: filename('js'),
     path: path.resolve(__dirname, 'dist')
   },
   plugins: [
     new HTMLWebpackPlugin({
-      template: './index.html'
+      template: './index.html',
+      minify: {
+        collapseWhitespace: isProd
+      }
     }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, "src/favicon"), to: path.resolve(__dirname, "dist/favicon") },
+        { from: path.resolve(__dirname, "src/icons"), to: path.resolve(__dirname, "dist/icons") },
+        { from: path.resolve(__dirname, "src/img"), to: path.resolve(__dirname, "dist/img") }
+      ]
+    }),
+    new MiniCssExtractPlugin({
+      filename: filename('css')
+    })
   ],
+  optimization: optimization(),
+  devServer: {
+    port: 8080,
+    hot: isDev
+  },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
       },
       {
         test: /\.(png|jpg|jpeg|gif|svg)$/,
         use: ['file-loader']
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name][ext]'
+        }
       }
     ]
   }
