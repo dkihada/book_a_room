58db455ce2dfa688667d0017fcf00dc0bfe6c07d
Denis Agureev <dkihada@gmail.com>
2021-08-11 17:12:30 +0500

    add a plugin "jquery mask"

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,12 @@
+import jQuery from 'jquery';
+import './lib/jquery-1.9.0.min.js';
+import './lib/jquery.maskedinput.min.js';
+
 import '../styles/style.scss';
 import '../styles/ui/color-page.scss';
 import '../styles/ui/form-elements.scss';
+
+jQuery(function($){
+  $(".date").mask("99.99.9999");
+  $("#phone").mask("(999) 999-9999");
+});
